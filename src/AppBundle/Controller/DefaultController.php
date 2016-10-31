78fd80ebb3327f9718dcefa5c6548472d6d123a7
mahachaib <chaibmahaa@gmail.com>
2016-10-31 21:54:17 +0100

    first commit

---
+++ src/AppBundle/Controller/DefaultController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+
+class DefaultController extends Controller
+{
+    /**
+     * @Route("/", name="homepage")
+     */
+    public function indexAction(Request $request)
+    {        
+		$em = $this->getDoctrine()->getManager();
+		$users = $em->getRepository('AppBundle:User')->findAll();
+
+        return $this->render('AppBundle::index.html.twig', array('users' => $users));
+    }
+}
